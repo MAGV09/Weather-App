88b5fdc35eb58d48b97158e086baae0a563adc16
MAGV09 <mohamedadel2002320@gmail.com>
2026-01-31 07:25:25 +0200

    feat: add page layout and styling

--- src/script.js
+++ src/script.js
@@ -1,3 +1,35 @@
 import 'modern-normalize/modern-normalize.css';
 import './styles/reset.css';
 import './styles/style.css';
+
+async function getTemp(location, unit) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&include=days%2Ccurrent%2Calerts&key=7TJFYQFH929XUG52Y9GHDZF92&contentType=json`,
+    );
+    const weather = await response.json();
+    console.log(weather);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getTemp('ismailia', 'metric');
+
+//current
+/*
+ temp
+ feels like
+ wind
+ humdidity
+ condition
+ time
+ day
+ icon
+ */
+
+//week
+/*
+ min/max temp
+ icon
+ day
+ */
